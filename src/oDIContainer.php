12ef7625b0235f68862a67deb2283cd8cfeedbdb
Nate Obray <nathanobray@gmail.com>
2018-01-01 18:25:42 +0000

    update DI container

--- src/oDIContainer.php
+++ src/oDIContainer.php
@@ -1,8 +1,37 @@
 <?php
 	namespace obray
-	Class oDIContainer {
+	Class oDIContainer implements \psr\container\ContainerInterface {
 
-		
+		protected $dependencies;
+
+		public function __construct( $path )
+		{
+			if( file_exists($path) ){
+				$this->dependencies = include $path;
+				return;
+			}
+			throw new \Exeception("DI Container config file ".$path." not found");
+		}
+
+		public function get( string $entry )
+		{
+
+			if( $this->has($entry) )
+			{
+				return $this->dependencies[$entry];
+			}
+
+		}
+
+		public function has( string $entry )
+		{
+
+			if( in_array( $entry, $this->dependencies) )
+			{
+				return TRUE;
+			}
+			return FALSE;
+		}
 
 	}
 ?>
